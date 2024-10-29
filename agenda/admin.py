aab0645278dd1587b50b0221c9b820fd67d34e7b
Joseph Ortiz <Se_sema_95@hotmail.com>
2024-10-29 15:11:48 +0000

    create user on site and in admin panel to assess CRUD

--- agenda/admin.py
+++ agenda/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from .models import Event
-from  django_summernote.admin import SummernoteModelAdmin
+from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Event)
 class EventAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'start_date')
-    search_fields = ['start_date']
-    list_filter = ('start_date',)
+    search_fields = ['title', 'start_date']
+    list_filter = ('title', 'start_date',)
     summernote_fields = ('description',)
 
 
--- contact/admin.py
+++ contact/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import About, BookingRequest
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-admin.site.register(About)
+@admin.register(About)
+class AboutAdmin(SummernoteModelAdmin):
+    summernote_fields = ('title', 'content')
 
-admin.site.register(BookingRequest)
+@admin.register(BookingRequest)
+class BookingRequestAdmin(SummernoteModelAdmin):
+    list_display = ('date_request', 'time_request', 'guests')
+    search_fields = ('full_name', 'date_request', 'email')
+    list_filter = ('date_request', 'time_request', 'guests', 'full_name',)
